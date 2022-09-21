f8b712ef9b085421b61cd8188c689c7601c2f7c9
nathanchandler26 <64334357+nathanchandler26@users.noreply.github.com>
2022-09-20 22:13:06 -0600

    onScannedRobot

--- TopGun.java
+++ TopGun.java
@@ -18,7 +18,7 @@ public class TopGun extends Robot
 		// After trying out your robot, try uncommenting the import at the top,
 		// and the next line:
 
-		setColors(Color.blue,Color.black,Color.red); // body,gun,radar
+		setColors(Color.black,Color.darkGray,Color.blue); // body,gun,radar -- Set the colors
 
 		// Robot main loop
 		while(true) {
@@ -37,24 +37,39 @@ public class TopGun extends Robot
 	 */
 	public void onScannedRobot(ScannedRobotEvent e) {
 		// Replace the next line with any behavior you would like
-		if (getEnergy() > 50) // If the energy level is over 50 do this
+		double Energy =  e.getEnergy(); // Gets the energy of TopGun
+		double Distance = e.getDistance(); // Gets the dstance of the scanned robot		
+
+		if (Energy > 50) // If the energy level is over 50 do this
 		{ 
-			if (e.getDistance() < 50) // If enemy is less than 50 away do this
+			if (Distance < 50) // If enemy is less than 50 away do this
 			{
-				fire(1);
+				fire(5);
 			}
-			else if (e.getDistance() < 100) // If enemy is less than 100 away do this
+			else if (Distance < 100) // If enemy is less than 100 away do this
+			{
+				fire(4);
+			}
+			else if (Distance < 150) // If enemy is less than 150 away do this
+			{
+				fire(3);
+			}
+			else if (Distance < 200) // If enemy is less than 200 away do this
 			{
 				fire(2);
-			}
+			}	
 			else
 			{
-				fire(3); // If enemy is over 100 away do this
+				fire(1); // If enemy is over 200 away do this
 			}
 		}
-		else // If the energy level is below 50 do this
+		else if (Energy > 25) // If the energy level is < 50 and > 25
 		{
-			fire(5);
+			fire(1);
+		}
+		else // If energy is less than 25
+		{
+			fire(.5);
 		}
 	}
 
